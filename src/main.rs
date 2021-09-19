480bccd1c6da7e556d8c131b08692a0b2041b4d5
Alexandre Monjol <alexandre.monjol@qonto.com>
2021-09-19 18:04:02 +0200

    [FEAT] Get user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io;
+
 fn main() {
     let word_to_guess = String::from("table");
     println!("Word to guess: {}", &typed_word);
+    // Get user input
+    let mut input = String::new();
+    io::stdin()
+    .read_line(&mut input)
+    .ok()
+    .expect("Failed to read user input.");
 }
