f9667cfec11cec4d4021704ceebd8d26ba1ff483
Alexandre Monjol <alexandre.monjol@qonto.com>
2021-09-19 18:06:40 +0200

    [FEAT] Prepare strings to be comparable

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::io;
 fn main() {
     let word_to_guess = String::from("table");
     let mut is_valid = false;
+    let mut typed_word = String::new();
+    for _ in 0..word_to_guess.len() { typed_word.push('_') }
+
+
     println!("Word to guess: {}", &typed_word);
+
     while !is_valid {
         println!("Please enter a letter");
         
@@ -13,5 +18,9 @@ fn main() {
         .read_line(&mut input)
         .ok()
         .expect("Failed to read user input.");
+
+        // Clean input backline
+        let len = input.trim_end_matches(&['\r', '\n'][..]).len();
+        input.truncate(len);
     }
 }
