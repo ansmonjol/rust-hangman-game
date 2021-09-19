715f180321a4d5453a4f33e6769694c8ea7d2fb2
Alexandre Monjol <alexandre.monjol@qonto.com>
2021-09-19 18:05:38 +0200

    [FEAT] Loop over user input

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::io;
 
 fn main() {
     let word_to_guess = String::from("table");
+    let mut is_valid = false;
     println!("Word to guess: {}", &typed_word);
-    // Get user input
-    let mut input = String::new();
-    io::stdin()
-    .read_line(&mut input)
-    .ok()
-    .expect("Failed to read user input.");
+    while !is_valid {
+        println!("Please enter a letter");
+        
+        // Get user input
+        let mut input = String::new();
+        io::stdin()
+        .read_line(&mut input)
+        .ok()
+        .expect("Failed to read user input.");
+    }
 }
